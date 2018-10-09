155cb7e79276c62be2dbff4dcf98921ea80c1ebe
giuliovisco <giulio.visco@iongroup.com>
2018-10-09 17:46:35 +0200

    simplified UI and key bindings

--- albumen.py
+++ albumen.py
@@ -3,8 +3,9 @@
 import sys
 import urllib.request
 
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (QWidget, QDesktopWidget, QApplication, QHBoxLayout, 
-    QVBoxLayout, QPushButton, QLineEdit, QLabel)
+    QVBoxLayout, QPushButton, QLineEdit, QLabel, QInputDialog)
 from PyQt5.QtGui import QIcon, QImage, QPixmap
 
 
@@ -16,13 +17,18 @@ class Albumen(QWidget):
         
         
     def initUI(self):
-        hbox = QHBoxLayout()
-        hbox.addWidget(QLineEdit(self), 3)
-        hbox.addWidget(QPushButton("Search"), 1)
+        self.setLayout(self.initLayout()) 
+
+        self.resize(500, 500)
+        self.centerWindow()
+        self.setWindowTitle('Albumen')
+        self.setWindowIcon(QIcon('icon.png'))
+
+        self.show()
 
 
-        vbox = QVBoxLayout()
-        vbox.addLayout(hbox)
+    def initLayout(self):
+        mainbox = QVBoxLayout()
         
         url = 'https://lastfm-img2.akamaized.net/i/u/300x300/17ea9e6cf87d4c04a622b5bf7ba241be.png'
         data = urllib.request.urlopen(url).read()
@@ -31,23 +37,33 @@ class Albumen(QWidget):
         lbl = QLabel(self)
         lbl.setScaledContents(True)
         lbl.setPixmap(QPixmap(image))
-        vbox.addWidget(lbl)
-        #vbox.addStretch(1)
+        mainbox.addWidget(lbl)
+        #mainbox.addStretch(1)
+        return mainbox
 
-        self.setLayout(vbox) 
 
-        self.resize(500, 500)
-        self.center()
-        self.setWindowTitle('Albumen')
-        self.setWindowIcon(QIcon('icon.png'))
-
-        self.show()
-
-    def center(self):
+    def centerWindow(self):
         qr = self.frameGeometry()
         cp = QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
+
+
+    def keyPressEvent(self, e):  
+        print(e.key())  
+        if e.key() == Qt.Key_Escape:
+            self.closeApplication()
+        elif e.key() == Qt.Key_Return:
+            self.search()
+
+    def search(self):
+        text, ok = QInputDialog.getText(self, 'Input Dialog', 'Search top albums')
+        if ok:
+            print(str(text))
+
+
+    def closeApplication(self):
+        QApplication.instance().quit()
         
 if __name__ == '__main__':
     
