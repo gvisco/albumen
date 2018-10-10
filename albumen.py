1efc9d9ed63c3d570e89f5202912d35a5c9d24af
giuliovisco <giulio.visco@iongroup.com>
2018-10-10 19:24:04 +0200

    display multiple albums

--- albumen.py
+++ albumen.py
@@ -15,9 +15,12 @@ class Albumen(QWidget):
     
     def __init__(self):
         super().__init__()
-        self.initUI()
+        self.albums = []
+        self.artist = '-'
+        self.index = 0
         self.network = self.initLastFM()
-                
+        self.initUI()
+
     def initLastFM(self):
         config = configparser.ConfigParser()
         config.read('config.ini')
@@ -37,19 +40,23 @@ class Albumen(QWidget):
         self.mainlabel = QLabel(self)
         mainbox.addWidget(self.mainlabel)
         #mainbox.addStretch(1)
-        self.updateContent('-')
+        self.updateContent()
         return mainbox
 
-    def updateContent(self, artist, url=None):
+    def updateContent(self):
         image = QImage()
-        if url:
+        if self.albums:
+            album = self.albums[self.index].item
+            url = album.get_cover_image()
             data = urllib.request.urlopen(url).read()
             image.loadFromData(data)
+            title = '%s - #%i %s' % (self.artist, self.index + 1, album.get_title())
         else:
             image.load('question-mark.png')
+            title = "-"
         self.mainlabel.setScaledContents(True)
         self.mainlabel.setPixmap(QPixmap(image))
-        self.setWindowTitle(artist)
+        self.setWindowTitle(title)
 
     def centerWindow(self):
         qr = self.frameGeometry()
@@ -63,24 +70,42 @@ class Albumen(QWidget):
             self.closeApplication()
         elif e.key() == Qt.Key_Return:
             self.searchDialog()
+        elif e.key() == Qt.Key_Right:
+            self.nextAlbum()
+        elif e.key() == Qt.Key_Left:
+            self.previousAlbum()
 
     def searchDialog(self):
         text, ok = QInputDialog.getText(self, 'Search', 'Search top album')
         if ok: 
             self.searchAlbums(text)
 
-    def searchAlbums(self, name):
-        artist = self.network.get_artist(name)
-        topalbum = artist.get_top_albums(1)[0]
+    def nextAlbum(self):
+        if self.albums:
+            self.index = (self.index + 1) % len(self.albums) 
+            self.updateContent()
+
+    def previousAlbum(self):
+        if self.albums:
+            self.index = (self.index - 1) % len(self.albums) 
+            self.updateContent()
+
+    def searchAlbums(self, name, limit=5):
+        result = self.network.get_artist(name)
+        
+        self.artist = result.get_correction()
+        self.albums = result.get_top_albums(limit)
+        self.index = 0
+        
+        topalbum = self.albums[self.index].item if self.albums else None
+        self.updateContent()
         # TODO: error handling
-        self.updateContent(name, topalbum.item.get_cover_image())
 
     def closeApplication(self):
         QApplication.instance().quit()
 
 
 if __name__ == '__main__':
-    
     app = QApplication(sys.argv)
     ex = Albumen()
     sys.exit(app.exec_())
